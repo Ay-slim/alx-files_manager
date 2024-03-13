fe27eb735d2b35db77b898c3ac19e5227731e340
Ay-slim <ayooluwaadedipe@gmail.com>
2024-03-13 20:28:34 +0000

    user controller and route'

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 // import AuthController from '../controllers/AuthController';
 // import FilesController from '../controllers/FilesController';
-// import UsersController from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
@@ -12,8 +12,8 @@ router.get('/stats', AppController.getStats);
 // router.get('/connect', AuthController.getConnect);
 // router.get('/disconnect', AuthController.getDisconnect);
 
-// router.post('/users', UsersController.postNew);
-// router.get('/users/me', UsersController.getMe);
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 
 // router.post('/files', FilesController.postUpload);
 // router.get('/files/:id', FilesController.getShow);
