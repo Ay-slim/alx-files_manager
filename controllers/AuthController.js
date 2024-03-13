715ecfe97bcff16a64f14ec5c23087061da7b849
Ay-slim <ayooluwaadedipe@gmail.com>
2024-03-13 20:32:55 +0000

    Auth routes and controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,34 @@
+import sha1 from 'sha1';
+import { v4 as uuid } from 'uuid';
+
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+export default class AuthController {
+  static async getConnect(request, response) {
+    const auth = request.header('Authorization') || '';
+    const buff = Buffer.from(auth.replace('Basic ', ''), 'base64');
+    const credentials = {
+      email: buff.toString('utf-8').split(':')[0],
+      password: buff.toString('utf-8').split(':')[1],
+    };
+    if (!credentials.email || !credentials.password) return response.status(401).send({ error: 'Unauthorized' });
+    const user = await (await dbClient.usersCollection()).findOne({ email: credentials.email });
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    if (sha1(credentials.password) !== user.password) return response.status(401).send({ error: 'Unauthorized' });
+    const token = uuid();
+    const key = `auth_${token}`;
+    await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
+    return response.status(200).send({ token });
+  }
+
+  static async getDisconnect(request, response) {
+    const token = request.header('X-Token');
+    if (!token) return response.status(401).send({ error: 'Unauthorized' });
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
+    await redisClient.del(key);
+    return response.status(204).send();
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-// import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 // import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 
@@ -9,8 +9,8 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-// router.get('/connect', AuthController.getConnect);
-// router.get('/disconnect', AuthController.getDisconnect);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
