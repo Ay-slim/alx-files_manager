2a4ed689ae895a276a3a7f0a956a5bd7ebb2725e
Ay-slim <ayooluwaadedipe@gmail.com>
2024-03-13 20:19:09 +0000

    App controller and routes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express';
-// import router from './routes';
+import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '200mb' }));
-// app.use(router);
+app.use(router);
 
 app.listen(port);
 console.log(`Server running on port ${port}`);
