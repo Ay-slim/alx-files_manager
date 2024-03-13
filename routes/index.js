16fb6b432d2377ec8bf213a971ecbff7ee733f64
Ay-slim <ayooluwaadedipe@gmail.com>
2024-03-13 20:51:58 +0000

    File controller and routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
-// import FilesController from '../controllers/FilesController';
+import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
@@ -15,13 +15,13 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 
-// router.post('/files', FilesController.postUpload);
-// router.get('/files/:id', FilesController.getShow);
-// router.get('/files', FilesController.getIndex);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
-// router.put('/files/:id/publish', FilesController.putPublish);
-// router.put('/files/:id/unpublish', FilesController.putUnPublish);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnPublish);
 
-// router.get('/files/:id/data', FilesController.getFile);
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
